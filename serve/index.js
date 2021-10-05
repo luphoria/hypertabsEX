71e7d0244bb5fc4ef1b892d33e265eb45bdd8175
luphoria <60309933+luphoria@users.noreply.github.com>
2021-10-05 09:41:54 -0500

    Update index.js

--- serve/index.js
+++ serve/index.js
@@ -16,4 +16,4 @@ proxy.bundleScripts();
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
     response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(443);
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(80);
